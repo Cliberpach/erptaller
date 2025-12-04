fa981d0a54ddd0be3e32f9750071992342cf9d1c
Daniel Alva <luisdaniel9986@gmail.com>
2025-12-03 19:00:26 -0500

    update mgirations

--- database/migrations/tenant/2023_10_02_143242_create_petty_cash_books_table.php
+++ database/migrations/tenant/2023_10_02_143242_create_petty_cash_books_table.php
@@ -13,18 +13,32 @@ return new class extends Migration
     {
         Schema::create('petty_cash_books', function (Blueprint $table) {
             $table->id();
+
             $table->unsignedBigInteger('petty_cash_id');
             $table->foreign('petty_cash_id')->references('id')->on('petty_cashes')->onDelete('cascade');
-            $table->string('status')->default('open');;
+
             $table->unsignedBigInteger('shift_id');
             $table->foreign('shift_id')->references('id')->on('shifts');
+
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->decimal('initial_amount',10,2);
+
+            $table->unsignedBigInteger('creator_user_id')->nullable();
+            $table->string('creator_user_name')->nullable();
+            $table->unsignedBigInteger('editor_user_id')->nullable();
+            $table->string('editor_user_name')->nullable();
+            $table->unsignedBigInteger('delete_user_id')->nullable();
+            $table->string('delete_user_name')->nullable();
+
+            $table->string('name');
+            $table->enum('status', ['ANULADO', 'ABIERTO', 'CERRADO'])->default('CERRADO');
+            $table->decimal('initial_amount', 10, 2);
             $table->decimal('closing_amount', 10, 2)->nullable();
             $table->datetime('initial_date');
             $table->datetime('final_date')->nullable();
-            $table->decimal('sale_day',10,2)->nullable();
+            $table->decimal('sale_day', 10, 2)->nullable();
+
+            $table->timestamps();
         });
     }
 
