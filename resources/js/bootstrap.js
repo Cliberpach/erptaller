b5089bcfcb528fd2e487015d47c46c24a87e0dd2
Daniel Alva <luisdaniel9986@gmail.com>
2026-01-07 19:45:43 -0500

    test

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -86,7 +86,7 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 Pusher.logToConsole = true;
 
-window.Echo = new Echo({
+/*window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: 'mt1',
@@ -96,16 +96,22 @@ window.Echo = new Echo({
     encrypted: false,
     disableStats: true,
     enabledTransports: ['ws'],
+});*/
+
+
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+
+    wsHost: import.meta.env.VITE_PUSHER_HOST ?? window.location.hostname,
+    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+
+    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    encrypted: true,
+
+    disableStats: true,
+    enabledTransports: ['ws', 'wss'],
 });
-
-
-//window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
